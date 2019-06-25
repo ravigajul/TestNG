b39a8dfb6f707d8ba65669117b1333959f337c7b
ravigajul <ravi.gajul@yahoo.com>
2019-06-25 23:51:52 +0530

    converted TestNG to Maven project and added sure fire pluggin

--- src/TestNG/TestNGDemo.java
+++ src/TestNG/TestNGDemo.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
