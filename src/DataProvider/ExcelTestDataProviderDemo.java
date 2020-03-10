3686b6b0aaca9ba26902cf1a2974025b1920916c
ravigajul <51070896+ravigajul@users.noreply.github.com>
2020-03-10 13:02:24 +0530

    Update ExcelTestDataProviderDemo.java
    made changes to data provider to ready only rows with Runmode='Y'

--- src/DataProvider/ExcelTestDataProviderDemo.java
+++ src/DataProvider/ExcelTestDataProviderDemo.java
@@ -29,23 +29,26 @@ public class ExcelTestDataProviderDemo {
 
 	}
 
-	@DataProvider(name = "testdataprovider")
+		@DataProvider(name = "testdataprovider")
 	public Object[][] getExcelData() {
 		String fileName = System.getProperty("user.dir") + "\\src\\test\\java\\com\\resources\\testdata\\TestData.xlsx";
 		String sheetName = "Sheet1";
 		Object[][] obj1 = null;
+		Hashtable<Integer, Object> ht = new Hashtable<Integer, Object>();
 		try {
 			fs = new FileInputStream(fileName);
 			wb = new XSSFWorkbook(fs);
 			sh = wb.getSheet(sheetName);
 			int columncount = sh.getRow(0).getLastCellNum();
 			int rowcount = sh.getPhysicalNumberOfRows();
-			obj1 = new Object[rowcount-1][1];
+			//Object obj1= new Object[rowcount-1][1]
+			
 			int i = 0;
 			Iterator<Row> rowiterator = sh.iterator();
 
 			while (rowiterator.hasNext()) {
 				Row row = rowiterator.next();
+				
 				if (i != 0) {
 					System.out.println(row.getRowNum());
 					Iterator<Cell> cellIterator = row.cellIterator();
@@ -54,31 +57,78 @@ public class ExcelTestDataProviderDemo {
 
 					while (cellIterator.hasNext()) {
 						Cell cell = cellIterator.next();
-						if (cell.getCellType() == CellType.STRING) {
-							hashtable.put(sh.getRow(0).getCell(j).getStringCellValue(),
-									sh.getRow(i).getCell(j).getStringCellValue());
+						switch (cell.getCellType()) {
+						case STRING:
+							try{
+								hashtable.put(sh.getRow(0).getCell(j).getStringCellValue(),
+							
+									sh.getRow(i).getCell(j,Row.MissingCellPolicy.CREATE_NULL_AS_BLANK).getStringCellValue());
+							}catch(Exception e)
+							{
+								e.getMessage();
+							}
 							System.out.println(
 									cell.getCellType() + "--->" + sh.getRow(i).getCell(j).getStringCellValue());
-						} else if (cell.getCellType() == CellType.NUMERIC) {
+							break;
+						case NUMERIC:
 							hashtable.put(sh.getRow(0).getCell(j).getStringCellValue(),
-									sh.getRow(i).getCell(j).getStringCellValue());
+									sh.getRow(i).getCell(j,Row.MissingCellPolicy.CREATE_NULL_AS_BLANK).getStringCellValue());
 							System.out.println(
 									cell.getCellType() + "--->" + sh.getRow(i).getCell(j).getStringCellValue());
+						case BLANK:
+							hashtable.put(sh.getRow(0).getCell(j).getStringCellValue(),
+									sh.getRow(i).getCell(j,Row.MissingCellPolicy.CREATE_NULL_AS_BLANK).getStringCellValue());
+							System.out.println(
+									cell.getCellType() + "--->" + sh.getRow(i).getCell(j).getStringCellValue());
+						default:
+							break;
 						}
 						j++;
 					}
-					obj1[i-1][0] = hashtable;
+					//obj1[i - 1][0] = hashtable;
+					ht.put(i,hashtable);
 
 				}
 				i++;
+
 			}
 			// System.out.println(cell.getStringCellValue());
 
-		} catch (FileNotFoundException e) {
+		} catch (
+
+		FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 			e.printStackTrace();
+		}
+		
+		//removing rows with run mode='N'
+		
+		Set<Integer> keys = ht.keySet();	
+	    //Obtaining iterator over set entries
+	    Iterator<Integer> itr = keys.iterator();
+	    while (itr.hasNext()) {
+			Integer integer = (Integer) itr.next();
+			 System.out.println("Key: "+integer.toString()+" & Value: "+ht.get(integer));
+			Hashtable tempht=(Hashtable) ht.get(integer);
+			if(tempht.get("RunMode").toString().equalsIgnoreCase("N")) {
+				itr.remove();
+				//ht.remove(integer); //this will throw concurrent modification exception
+				System.out.println("removed key" + integer);
+			}
+		}
+	    System.out.println(ht.toString());
+	    System.out.println(ht.size());
+	    
+	    obj1= new Object[ht.size()][1];
+	    Iterator<Integer> itr1 = keys.iterator();
+	    int k=0;
+	    while (itr1.hasNext()) {
+			Integer integer = (Integer) itr1.next();
+			 System.out.println("Key: "+integer.toString()+" & Value: "+ht.get(integer));
+			obj1[k][0]=ht.get(integer);
+			k++;
 		}
 		return obj1;
 
